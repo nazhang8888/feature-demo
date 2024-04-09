6c4099198d4e2872d8ff8cc9a73a903f0b4db61d
Nicholas Chang <nazhang@pm.me>
2024-04-09 18:25:15 -0400

    added inline obj as a sample

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -8,12 +8,39 @@
     :rows="[
       {
         id: 1,
+        name: 'M855',
+        caliber: '5.56x45mm NATO',
+        origin: 'US',
       },
       {
         id: 2,
+        name: '7.62x51mm NATO',
+        caliber: '7.62x51mm NATO',
+        origin: 'US',
       },
       {
         id: 3,
+        name: '7.62x39mm ',
+        caliber: '7.62x39mm',
+        origin: 'USSR',
+      },
+      {
+        id: 4,
+        name: '.50 BMG',
+        caliber: '12.7x99mm NATO',
+        origin: 'US',
+      },
+      {
+        id: 5,
+        name: '.45 ACP',
+        caliber: '11.43x23mm',
+        origin: 'US',
+      },
+      {
+        id: 6,
+        name: '9x19 Parabellum',
+        caliber: '9x19mm',
+        origin: 'US',
       },
     ]"
     :columns="[
@@ -24,8 +51,29 @@
         align: 'left',
         sortable: true,
       },
+      {
+        name: 'name',
+        label: 'Name',
+        field: 'name',
+        align: 'left',
+        sortable: true,
+      },
+      {
+        name: 'caliber',
+        label: 'Caliber',
+        field: 'caliber',
+        align: 'left',
+        sortable: true,
+      },
+      {
+        name: 'origin',
+        label: 'Origin',
+        field: 'origin',
+        align: 'left',
+        sortable: true,
+      },
     ]"
-    :visible-columns="['id']"
+    :visible-columns="['name', 'caliber', 'origin']"
   >
   </q-table>
 </template>
