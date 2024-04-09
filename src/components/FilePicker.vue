a200c08589ea8a8ffd4c4279233eeb6c2b7c1dc6
Nicholas Chang <nazhang@pm.me>
2024-04-09 16:53:37 -0400

    component for gallery

---
+++ src/components/FilePicker.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-file
+    v-model="files"
+    label="Pick files"
+    filled
+    counter
+    multiple
+    style="max-width: 300px"
+    class="col q-pa-md"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+defineOptions({
+  name: "FilePicker",
+});
+
+const files = ref(null);
+</script>
--- src/pages/GalleryPage.vue
+++ src/pages/GalleryPage.vue
@@ -1,9 +1,10 @@
 <template>
   <q-page class="fit">
-    <div class="row items-center">
-      <DateRangePicker class="col" />
-      <ColorPicker class="col justify-center" />
-      <CarouselDialog class="col flex justify-center" />
+    <div class="row items-center justify-around">
+      <DateRangePicker class="col-md-auto" />
+      <ColorPicker class="col-md-auto justify-center" />
+      <CarouselDialog class="col-md-auto flex justify-center" />
+      <FilePicker class="col-md-auto flex justify-center" />
     </div>
   </q-page>
 </template>
@@ -12,9 +13,10 @@
 import DateRangePicker from "../components/DateRangePicker.vue";
 import ColorPicker from "../components/ColorPicker.vue";
 import CarouselDialog from "../components/CarouselDialogue.vue";
+import FilePicker from "../components/FilePicker.vue";
 
 defineOptions({
   name: "GalleryPage",
-  components: { DateRangePicker, ColorPicker, CarouselDialog },
+  components: { DateRangePicker, ColorPicker, CarouselDialog, FilePicker },
 });
 </script>
