ece4aea528f641ad7a6b06a050699fdc97c1f178
Nicholas Chang <nazhang@pm.me>
2024-04-22 15:10:24 -0400

    initial ts refactor

--- src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import { store } from "quasar/wrappers";
-import { createPinia } from "pinia";
-
-export default store((/* { ssrContext } */) => {
-  const pinia = createPinia();
-  // e.g. pinia.use(SomePiniaPlugin)
-
-  return pinia;
-});
