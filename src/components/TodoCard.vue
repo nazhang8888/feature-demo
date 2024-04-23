4e913949b2873b8c7a61e5801a9f8fa20830b474
Nicholas Chang <nazhang@pm.me>
2024-04-22 20:44:15 -0400

    fix: defined state

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -3,19 +3,19 @@
     <q-list bordered>
       <q-item v-ripple class="justify-start">
         <q-item-label class="item-center self-center col-grow">
-          {{ todo.title }}
+          {{ todo!.title }}
         </q-item-label>
         <div class="row">
           <q-item-section avatar class="q-pr-none q-pl-md items-center">
             <q-icon
-              @click="todoStore.toggleFavorite(todo.id)"
+              @click="todoStore.toggleFavorite(todo!.id)"
               name="favorite"
-              :class="{ activefavorite: todo.isFavorite }"
+              :class="{ activefavorite: todo!.isFavorite }"
             />
             Favorite
           </q-item-section>
           <q-item-section avatar class="q-pr-none q-pl-md items-center">
-            <q-icon @click="todoStore.finishTodo(todo.id)" name="done" />
+            <q-icon @click="todoStore.finishTodo(todo!.id)" name="done" />
             Done
           </q-item-section>
         </div>
