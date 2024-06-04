41606f605fa9704b2ea4abb05dd4c80d3d748c2f
Nicholas Chang <nazhang@pm.me>
2024-06-04 15:50:08 -0400

    fast forward

--- test/vitest/__tests__/InteractiveMapComponent.test.ts
+++ test/vitest/__tests__/InteractiveMapComponent.test.ts
@@ -11,37 +11,6 @@ import { installRouter } from './mocks/install-router';
 
 import InteractiveMapComponent from '@/components/InteractiveMap.vue';
 
-import type { Mock } from 'vitest';
-import type { Store, StoreDefinition } from 'pinia';
-import { ComputedRef } from 'vue';
-
-function mockedStore<TStoreDef extends () => unknown>(
-  useStore: TStoreDef
-): TStoreDef extends StoreDefinition<
-  infer Id,
-  infer State,
-  infer Getters,
-  infer Actions
->
-  ? Store<
-      Id,
-      State,
-      Record<string, never>,
-      {
-        [K in keyof Actions]: Actions[K] extends (
-          ...args: infer Args
-        ) => infer ReturnT
-          ? // 👇 depends on your testing framework
-            Mock<Args, ReturnT>
-          : Actions[K];
-      }
-    > & {
-      [K in keyof Getters]: Getters[K] extends ComputedRef<infer T> ? T : never;
-    }
-  : ReturnType<TStoreDef> {
-  return useStore() as any;
-}
-
 installQuasarPlugin();
 Object.defineProperty(window, 'localStorage', {
   value: localStorage as Storage,
@@ -71,7 +40,7 @@ describe('InteractiveMap Component', () => {
         ],
       },
     });
-    const mapStore = mockedStore(useMapStore);
+    const mapStore = useMapStore();
     const tableStore = useTableStore();
     expect(mapStore).not.toBeNull();
     expect(tableStore).not.toBeNull();
